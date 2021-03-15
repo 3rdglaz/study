29a0f77a34cf355faaa6eacd15cdad57777513aa
Caio ntbk <pmscaio@gmail.com>
2021-03-15 03:05:52 -0300

    automação server diagramação

--- Scripts/server_automation.sh
+++ Scripts/server_automation.sh
@@ -1,6 +1,16 @@
-#! /bin/zsh	
-# instala arquivos
+#! /bin/bash
+## instala arquivos
+echo -e "\n\n      Instalando ferramentas principais ...     \n\n"
 sudo apt install net-tools vsftpd apache2 mysql-server postfix docker docker.io -y
+echo -e "\n\n      Baixando e aplicando atualizações ...     \n\n"
 sudo apt update -y
 sudo apt upgrade -y
+echo -e "\n\n         Finalizando instalação ...             \n\n"
 sudo apt install bind9 -y
+echo -e "\n\n    Não se esqueça de configurar o banco de dados\n\n"
+
+
+
+
+
+
