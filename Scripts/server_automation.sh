6d743cb7547d8eb4c3bcccc83d97a7e52fb0f306
Caio ntbk <pmscaio@gmail.com>
2021-03-15 02:34:21 -0300

    automação server

---
+++ Scripts/server_automation.sh
@@ -0,0 +1,6 @@
+#! /bin/zsh	
+# instala arquivos
+sudo apt install net-tools vsftpd apache2 mysql-server postfix docker docker.io -y
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install bin9 -y
