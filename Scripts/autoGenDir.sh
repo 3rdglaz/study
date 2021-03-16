e6afd861b19b2a4f6625e573b60045756b7aceb1
Caio ntbk <pmscaio@gmail.com>
2021-03-16 01:39:05 -0300

    auto criação de diretorio e commit no git

---
+++ Scripts/autoGenDir.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+if [ -z "$1"]
+then
+	echo "Uso: ./autoGenDir.sh <Nome>"
+	exit 1
+fi
+echo -e "\n    criando pasta...\n"
+mkdir ~/Documentos/0025/ALUNOS/$1
+touch $1/README.md
+git pull
+git add *
+git commit -m "upload nome"
+git push
+
