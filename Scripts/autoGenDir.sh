86d986dbbb9a503f525e7498b5e06e187c6be3a6
Caio ntbk <pmscaio@gmail.com>
2021-03-16 14:24:32 -0300

    incluso atualização no readme

--- Scripts/autoGenDir.sh
+++ Scripts/autoGenDir.sh
@@ -8,8 +8,9 @@ echo -e "\n    criando pasta...\n"
 mkdir ~/Documentos/0025/ALUNOS/$1
 cd ~/Documentos/0025/ALUNOS
 touch $1/README.md
+echo "   - [$1](https://github.com/3rdglaz/0025/tree/main/ALUNOS/$1)" >>     /home/anao/Documentos/0025/README.md
 git pull
-git add *
+git add ~/Documentos/0025/*
 git commit -m "upload nome"
 git push
 
